38a081be1c5a22a9781f3983c3c9cf7912384505
jeffasseur <r0846107@student.thomasmore.be>
2022-10-21 15:54:20 +0200

    feat: add CORS allowense for codepen

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -19,6 +21,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://codepen.io'],
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
